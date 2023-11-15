5a049d542ea796b8f02890817cd3549ecfe0ac11
Harrison613 <102319658+Harrison613@users.noreply.github.com>
2023-11-15 16:35:15 -0600

    Update TestApplication.js
    should be able to connect to google collab by sending a question and recieving and answer

--- src/TestApplication.js
+++ src/TestApplication.js
@@ -1,7 +1,37 @@
 import React from "react";
+import config from "./config";
 import { Link } from "react-router-dom";
 import image1 from "./images/image-from-rawpixel-id-3318351-original.jpg"
 function TestApplication(){
+
+    const [quesiton, setQuestion] = useState('');
+    const [answer, setAnswer] = useState('');
+
+    const handleQuestionChange = (event) => {
+        setQuestion(event.target.value);
+    }
+
+    const handleClear = () => {
+        setQuestion('');
+        setAnswer('');
+    }
+
+    const handleSubmit = async () => {
+        try {
+            const response = await fetch(`${config.ngrokUrl}/ask`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ query: question }),
+      });
+
+      const result = await response.json();
+      setAnswer(result.answer);
+    } catch (error) {
+      console.error('Error submitting question:', error);
+    }
+  };
     
     return(
         <div style={{ 
@@ -10,11 +40,21 @@ function TestApplication(){
             height: window.screen.height
           }}>
             <label class="qblabel">
-                <textarea class="questionbox" rows={5} col={200} placeholder="Input Question"/>
-                <button type="clear">Clear</button>
-                <button type="submit">Submit</button>
+                <textarea 
+                    class="questionbox" 
+                    rows={5}  
+                    cols={200} 
+                    placeholder="Input Question"
+                    value={question}
+                    onChange={handleQuestionChange}
+                />
+                <button type="clear" onClick={handleClear}>Clear</button>
+                <button type="submit" onClick={handleSubmit}>Submit</button>
             </label>
+        <div>
+            <strong>Answer:</strong> {answer}
         </div>
+    </div>
     );
 }
 
